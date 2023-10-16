731699f668439a49fd5f58d1b78f33c4c0675ef9
Robson Alves <robson.alves@outlook.com>
2023-10-16 20:52:12 -0300

    Ajustando mappers

--- src/main/java/com/postech/entregavel1techchallenge/adapters/out/customer/SaveCustomerAdapter.java
+++ src/main/java/com/postech/entregavel1techchallenge/adapters/out/customer/SaveCustomerAdapter.java
@@ -1,10 +1,9 @@
 package com.postech.entregavel1techchallenge.adapters.out.customer;
 
 import com.postech.entregavel1techchallenge.adapters.out.repository.CustomerRepository;
-import com.postech.entregavel1techchallenge.adapters.out.repository.entity.CustomerEntity;
+import com.postech.entregavel1techchallenge.adapters.out.repository.mapper.CustomerEntityMapper;
 import com.postech.entregavel1techchallenge.application.core.domain.customer.Customer;
 import com.postech.entregavel1techchallenge.application.ports.out.customer.SaveCustomerOutPort;
-import com.postech.entregavel1techchallenge.config.mapper.ModelMapperCustom;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,13 +14,13 @@ public class SaveCustomerAdapter implements SaveCustomerOutPort {
 
     private final CustomerRepository customerRepository;
 
-    private final ModelMapperCustom mapper;
+    private final CustomerEntityMapper entityMapper;
 
     @Override
     @Transactional
     public Customer save(Customer customer) {
-        var customerEntity = mapper.map(customer, CustomerEntity.class);
+        var customerEntity = entityMapper.toCustomerEntity(customer);
         customerRepository.save(customerEntity);
-        return mapper.map(customerEntity, Customer.class);
+        return entityMapper.toCustomer(customerEntity);
     }
 }
