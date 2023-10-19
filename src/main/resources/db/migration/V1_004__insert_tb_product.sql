b1fbe6f8edc90f22da43b850c8bf7ae1fb6afcef
Robson Alves <robson.alves@outlook.com>
2023-10-18 22:04:56 -0300

    Adicionando migration para carga inicial de produtos

---
+++ src/main/resources/db/migration/V1_004__insert_tb_product.sql
@@ -0,0 +1,47 @@
+-- Cadastrando produtos para categoria LANCHE
+INSERT INTO tb_product (id, name, category, description, images, amount, create_date, last_modified_date)
+VALUES ('4db78233-657a-4ed1-8abd-48688ecd83cd', 'Lanche 1', 'LANCHE', 'DESCRICAO PRODUTO 1', 'image 1,image 2', 25.00, current_date, current_date);
+INSERT INTO tb_product (id, name, category, description, images, amount, create_date, last_modified_date)
+VALUES ('c3af44d4-5684-4e88-9263-2450c2d40c63', 'Lanche 2', 'LANCHE', 'DESCRICAO PRODUTO 2', 'image 1,image 2', 30.00, current_date, current_date);
+INSERT INTO tb_product (id, name, category, description, images, amount, create_date, last_modified_date)
+VALUES ('5a005003-3a86-4588-8abc-aac1c63cfb02', 'Lanche 3', 'LANCHE', 'DESCRICAO PRODUTO 3', 'image 1,image 2', 35.00, current_date, current_date);
+INSERT INTO tb_product (id, name, category, description, images, amount, create_date, last_modified_date)
+VALUES ('f4bf006d-e7e1-4ca6-9391-045a1c572080', 'Lanche 4', 'LANCHE', 'DESCRICAO PRODUTO 4', 'image 1,image 2', 40.00, current_date, current_date);
+INSERT INTO tb_product (id, name, category, description, images, amount, create_date, last_modified_date)
+VALUES ('9b3f8bcc-5f46-47dd-972d-51d9ecac0f60', 'Lanche 5', 'LANCHE', 'DESCRICAO PRODUTO 5', 'image 1,image 2', 45.00, current_date, current_date);
+
+-- Cadastrando produtos para a categoria ACOMPANHAMENTO
+INSERT INTO tb_product (id, name, category, description, images, amount, create_date, last_modified_date)
+VALUES ('d1ac1b38-7fce-4179-a17b-5b2f49a0de0c', 'Acompanhamento 1', 'ACOMPANHAMENTO', 'DESCRICAO Acompanhamento 1', 'image 1,image 2', 10.00, current_date, current_date);
+INSERT INTO tb_product (id, name, category, description, images, amount, create_date, last_modified_date)
+VALUES ('22c6fb10-d234-472f-8aca-3a2ee1958c4d', 'Acompanhamento 2', 'ACOMPANHAMENTO', 'DESCRICAO Acompanhamento 2', 'image 1,image 2', 14.00, current_date, current_date);
+INSERT INTO tb_product (id, name, category, description, images, amount, create_date, last_modified_date)
+VALUES ('1587d157-0dc0-4ce0-8889-706b25669109', 'Acompanhamento 3', 'ACOMPANHAMENTO', 'DESCRICAO Acompanhamento 3', 'image 1,image 2', 18.00, current_date, current_date);
+INSERT INTO tb_product (id, name, category, description, images, amount, create_date, last_modified_date)
+VALUES ('9279f9be-1e89-49a8-ad25-5e876a9a59b4', 'Acompanhamento 4', 'ACOMPANHAMENTO', 'DESCRICAO Acompanhamento 4', 'image 1,image 2', 20.00, current_date, current_date);
+INSERT INTO tb_product (id, name, category, description, images, amount, create_date, last_modified_date)
+VALUES ('630fa1d1-efca-434b-8479-e5765fea50c0', 'Acompanhamento 5', 'ACOMPANHAMENTO', 'DESCRICAO Acompanhamento 5', 'image 1,image 2', 22.00, current_date, current_date);
+
+-- Cadastrando produtos para a categoria BEBIDA
+INSERT INTO tb_product (id, name, category, description, images, amount, create_date, last_modified_date)
+VALUES ('24a7c930-8ed9-41c2-b8ed-ed483dc8b1e0', 'Bebida 1', 'BEBIDA', 'DESCRICAO Bebida 1', 'image 1,image 2', 10.00, current_date, current_date);
+INSERT INTO tb_product (id, name, category, description, images, amount, create_date, last_modified_date)
+VALUES ('958780b6-55f9-4404-ba0d-f9ca0dd3a26e', 'Bebida 2', 'BEBIDA', 'DESCRICAO Bebida 2', 'image 1,image 2', 12.00, current_date, current_date);
+INSERT INTO tb_product (id, name, category, description, images, amount, create_date, last_modified_date)
+VALUES ('4907aae2-6818-4635-a7fd-04f914b6dc52', 'Bebida 3', 'BEBIDA', 'DESCRICAO Bebida 3', 'image 1,image 2', 14.00, current_date, current_date);
+INSERT INTO tb_product (id, name, category, description, images, amount, create_date, last_modified_date)
+VALUES ('06d406ad-e205-4b12-9007-39171713394f', 'Bebida 4', 'BEBIDA', 'DESCRICAO Bebida 4', 'image 1,image 2', 16.00, current_date, current_date);
+INSERT INTO tb_product (id, name, category, description, images, amount, create_date, last_modified_date)
+VALUES ('0c26057c-6891-4c88-9443-5aaeadb1d86b', 'Bebida 5', 'BEBIDA', 'DESCRICAO Bebida 5', 'image 1,image 2', 18.00, current_date, current_date);
+
+-- Cadastrando produtos para a categoria SOBREMESA
+INSERT INTO tb_product (id, name, category, description, images, amount, create_date, last_modified_date)
+VALUES ('f1637bec-be0b-4496-8cc9-ef97be41fff4', 'Sobremesa 1', 'SOBREMESA', 'DESCRICAO Sobremesa 1', 'image 1,image 2', 18.00, current_date, current_date);
+INSERT INTO tb_product (id, name, category, description, images, amount, create_date, last_modified_date)
+VALUES ('e35341c2-d45b-4488-b775-a1cb58e9fea4', 'Sobremesa 2', 'SOBREMESA', 'DESCRICAO Sobremesa 2', 'image 1,image 2', 22.00, current_date, current_date);
+INSERT INTO tb_product (id, name, category, description, images, amount, create_date, last_modified_date)
+VALUES ('0db72ba2-787a-44eb-b604-01d6a32a64bd', 'Sobremesa 3', 'SOBREMESA', 'DESCRICAO Sobremesa 3', 'image 1,image 2', 26.00, current_date, current_date);
+INSERT INTO tb_product (id, name, category, description, images, amount, create_date, last_modified_date)
+VALUES ('cc38deec-4164-4704-8edd-3e457b5d397d', 'Sobremesa 4', 'SOBREMESA', 'DESCRICAO Sobremesa 4', 'image 1,image 2', 30.00, current_date, current_date);
+INSERT INTO tb_product (id, name, category, description, images, amount, create_date, last_modified_date)
+VALUES ('6213c22b-b1b8-4c42-ab92-c95f5e163dd1', 'Sobremesa 5', 'SOBREMESA', 'DESCRICAO Sobremesa 5', 'image 1,image 2', 32.00, current_date, current_date);
